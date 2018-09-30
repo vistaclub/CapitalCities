50d1dbba4f041c20704502090d2bf1b44597437f
Jason Wong <jawong71@gmail.com>
2018-09-29 18:46:19 -0700

    first commit

---
+++ Project5/Pin.swift
@@ -0,0 +1,13 @@
+//
+//  Pin.swift
+//  Project5
+//
+//  Created by Jason Wong on 2018-08-25.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class Pin: NSObject {
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Captial Cities
